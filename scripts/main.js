05408b2ddd9046ec5e94f99ed02edc191139eb9b
jnboakye <jaeyblaq2131@gmail.com>
2025-02-27 14:31:31 +0000

    new update with animations

--- scripts/main.js
+++ scripts/main.js
@@ -24,6 +24,35 @@ var typed = new Typed('.multiple', {
     loop: true
 });
 
+
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('section');
+
+function getActiveSection() {
+    let activeSection = null;
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 100 && rect.bottom >= 100) {
+            activeSection = section.id;
+        }
+    });
+    return activeSection;
+}
+
+function updateNav() {
+    const activeSection = getActiveSection();
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${activeSection}`) {
+            link.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', updateNav);
+updateNav(); // Initial call to set the active link on page load
+
 function animateProgressBar(targetPercentage, duration) {
     const progressBar = document.getElementById('progressBar');
     let currentPercentage = 0;
