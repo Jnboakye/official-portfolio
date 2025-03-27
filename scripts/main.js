0dcdedf98db110db1a5944ed3ad5af81ae5272eb
jnboakye <jaeyblaq2131@gmail.com>
2025-03-27 17:08:20 +0000

    responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -89,5 +89,11 @@ window.addEventListener('scroll', function () {
 });
 
 
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+hamburgerBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
 
 
